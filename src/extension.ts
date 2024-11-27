e53c7b53eab24c3d43f1fd9ef995e7d44728efb5
Robin <hrnan@live.se>
2024-11-27 12:24:19 +0100

    adding logo and publisher

--- src/extension.ts
+++ src/extension.ts
@@ -18,32 +18,38 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function runCodeViewCommand() {
-	await hidePrimaryBar();
-	await hideTerminalView();
+  await hidePrimaryBar();
+  await hideTerminalView();
 }
 
 async function runTerminalViewCommand() {
-	await hidePrimaryBar();
+  await hidePrimaryBar();
   await showTerminalMaximizedView();
 }
 
 async function hidePrimaryBar() {
   await vscode.commands.executeCommand("workbench.view.explorer");
-  await vscode.commands.executeCommand("workbench.action.toggleSidebarVisibility");
+  await vscode.commands.executeCommand(
+    "workbench.action.toggleSidebarVisibility"
+  );
 }
 
 async function hideTerminalView() {
-  await vscode.commands.executeCommand("workbench.action.focusFirstEditorGroup");
+  await vscode.commands.executeCommand(
+    "workbench.action.focusFirstEditorGroup"
+  );
   await vscode.commands.executeCommand("terminal.focus");
   const terminals = vscode.window.terminals;
-  
+
   for (let index = 0; index < terminals.length; index++) {
     terminals[index].hide();
   }
 }
 
 async function showTerminalMaximizedView() {
-  await vscode.commands.executeCommand("workbench.action.focusFirstEditorGroup");
+  await vscode.commands.executeCommand(
+    "workbench.action.focusFirstEditorGroup"
+  );
   await vscode.commands.executeCommand("terminal.focus");
   await vscode.commands.executeCommand("workbench.action.toggleMaximizedPanel");
 }
